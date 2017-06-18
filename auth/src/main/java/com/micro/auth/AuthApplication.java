874e34e39eba1f2a2dd3d9d164f4e5cc07934aa8
Sergey Bezvershenko <sergey@albatros.net>
2017-06-18 21:26:10 +0200

    Main application class. Enable Spring boot and discovery client

---
+++ auth/src/main/java/com/micro/auth/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.micro.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Main application class
+ * enables spring boot and discovery(Eureka) client
+ *
+ * @author Sergey Bezvershenko
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+}
