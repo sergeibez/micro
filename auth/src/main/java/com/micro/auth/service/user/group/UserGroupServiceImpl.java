1a43fb4584ae36008d188f1ed826440b879dd605
Sergey Bezvershenko <sergei.bez@gmail.com>
2017-08-17 01:45:50 +0200

    Use UserRole.getAllRoles instead the recursion method

--- auth/src/main/java/com/micro/auth/service/user/group/UserGroupServiceImpl.java
+++ auth/src/main/java/com/micro/auth/service/user/group/UserGroupServiceImpl.java
@@ -1,12 +1,14 @@
 package com.micro.auth.service.user.group;
 
 import com.micro.auth.domain.user.UserGroup;
+import com.micro.auth.domain.user.UserGroupRole;
 import com.micro.auth.domain.user.UserRole;
 import com.micro.auth.repository.user.UserGroupRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Implementation of {@link UserGroupService}
@@ -35,26 +37,12 @@ public class UserGroupServiceImpl implements UserGroupService {
      */
     @Override
     public List<UserRole> getGroupRoles(long groupId) {
-        List<UserRole> roles = new ArrayList<>();
-
         UserGroup group = userGroupRepository.findById(groupId)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("Cannot find UserGroup by id '%s'", groupId)));
 
-        group.getRoles().forEach(it -> addRole(roles, it.getRole()));
-
-        return roles;
-    }
-
-    /**
-     * Recursive method. Add role to list and add roles from parent if parent is not <code>null</code>
-     *
-     * @param roles a list of roles
-     * @param role a role for adding to list
-     */
-    private void addRole(List<UserRole> roles, UserRole role) {
-        roles.add(role);
-        if (role.getParent() != null) {
-            addRole(roles, role.getParent());
-        }
+        return group.getRoles().stream()
+                .map(UserGroupRole::getRole)
+                .flatMap(UserRole::getAllRoles)
+                .collect(toList());
     }
 }
