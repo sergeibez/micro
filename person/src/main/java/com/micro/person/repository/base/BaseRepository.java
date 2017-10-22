f7cf89a69bb0b3e703db0b21f5064544ed6fd6d8
Sergey Bezvershenko <sergey@albatros.net>
2017-10-22 12:24:19 +0200

    Added base class for repositories

---
+++ person/src/main/java/com/micro/person/repository/base/BaseRepository.java
@@ -0,0 +1,16 @@
+package com.micro.person.repository.base;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+
+/**
+ * Base class for all repositories
+ *
+ * @author Sergey Bezvershenko
+ */
+@NoRepositoryBean
+public interface BaseRepository<Entity> extends JpaRepository<Entity, Long> {
+    Optional<Entity> findById(Long id);
+}
