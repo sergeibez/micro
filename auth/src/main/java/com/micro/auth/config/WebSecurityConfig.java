bc5599a5904e69d3f6b5afb12823745718e8966d
Sergey Bezvershenko <sergey@albatros.net>
2018-02-11 15:54:45 +0100

    spring boot 2: add Bean for authenticationManager

--- auth/src/main/java/com/micro/auth/config/WebSecurityConfig.java
+++ auth/src/main/java/com/micro/auth/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.micro.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,5 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
