c82ed7077815d4c332d01b0d43bbf60489b4fa05
Sergey Bezvershenko <sergey@albatros.net>
2018-02-11 16:07:31 +0100

    spring boot 2: comment Spring Cloud

--- person/src/main/java/com/micro/person/PersonApplication.java
+++ person/src/main/java/com/micro/person/PersonApplication.java
@@ -2,7 +2,6 @@ package com.micro.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class
@@ -10,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *
  * @author Sergey Bezvershenko
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class PersonApplication {
 	public static void main(String[] args) {
