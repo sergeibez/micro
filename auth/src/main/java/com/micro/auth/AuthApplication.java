782e590d39bc33d889e590483c504d38649ca00e
Sergey Bezvershenko <sergey@albatros.net>
2018-02-11 16:04:08 +0100

    spring boot 2: comment Spring Cloud

--- auth/src/main/java/com/micro/auth/AuthApplication.java
+++ auth/src/main/java/com/micro/auth/AuthApplication.java
@@ -2,7 +2,6 @@ package com.micro.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class
@@ -10,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *
  * @author Sergey Bezvershenko
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 	public static void main(String[] args) {
